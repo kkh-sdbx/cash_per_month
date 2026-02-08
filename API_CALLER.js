92d1d22d7130e78972c216590de5edd647a9a9dd
kkh-sdbx <kyuhanoasis@gmail.com>
2026-02-08 17:14:20 +0900

    쿼리스트링 해체분석

--- API_CALLER.js
+++ API_CALLER.js
@@ -3,11 +3,27 @@ import EVENT_TARGET_BUNDLE from "/EVENT_TARGETS.js";
 const axios = require('axios');
 const BASE_URL = "http://apis.data.go.kr/1230000/ao/PrcrmntReqInfoService/getPrcrmntReqInfoListCnstwkPPSSrch";
 const TYPE_JSON = "&type=json";
-
+//http://apis.data.go.kr/1230000/ao/PrcrmntReqInfoService/getPrcrmntReqInfoListCnstwkPPSSrch?
+/**
+ * 여기서부터 쿼리 스트링.
+inqryDiv=1
+&
+inqryBgnDt=201606010000
+&
+inqryEndDt=201606052359
+&
+pageNo=1
+&
+numOfRows=10
+&
+ServiceKey=인증키
+ * 
+ */
+// 
 const CALLAPI = ()=>{
 
     const testCall = ()=>{
-        
+
 
     };
 
